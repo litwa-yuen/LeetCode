760f56c74849e650cd5c0fec5c65015083ee23b1
litwa-yuen <litwa.yuen@gmail.com>
2017-01-18 13:34:30 -0500

    Create 70. Climbing Stairs.java

---
+++ 70. Climbing Stairs.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+        int [] memo = new int[n+1];
+        memo[1] = 1;
+        memo[2] = 2;
+        for(int i = 3; i < memo.length; i++) {
+            memo[i] = memo[i-1] + memo[i-2];
+        }
+        return memo[n];
+    }
+}
